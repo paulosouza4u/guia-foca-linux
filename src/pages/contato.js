cc5065e464f871d94f5fea71a0f6eba85db43df2
Paulo Souza <42076064+paulosouza4u@users.noreply.github.com>
2025-11-03 19:32:48 -0300

    update final

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -9,8 +9,37 @@ const Contato = () => {
      **********************************/
     return (
         <Layout>
-            <div className="debug">
-                Contato
+            <div className="container">
+                <div className="w-100 text-center my-5">
+                    <h1 className="display-1 mb-5">Contato</h1>
+                    <h4>Em breve retornamos sua mensagem.</h4>
+                    <form name="contact" method="POST" data-netlify="true" onSubmit={ (event) => {
+                        event.preventDefault();
+                        //alert("Mensagem enviada com sucesso!");
+                    } }
+                          className="card w-25 mx-auto my-5 border border-primary py-5 px-3 rounded">
+                        <div className="mb-3">
+                            <label htmlFor="name" className="form-label"><span
+                                className="text-danger me-1">*</span>Nome:</label>
+                            <input type="text" className="form-control" id="name" aria-describedby="emailHelp"
+                                   name="name" required/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="mail" className="form-label"><span
+                                className="text-danger me-1">*</span>Email:</label>
+                            <input type="email" className="form-control" id="mail" aria-describedby="emailHelp"
+                                   name="email" required/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="textarea" className="form-label"><span className="text-danger me-1">*</span>Mensagem:</label>
+                            <textarea className="form-control" id="textarea" rows="3" name="message"
+                                      required></textarea>
+                        </div>
+                        <span><small><span className="text-danger me-1">*</span>Campo obrigatório.</small></span>
+                        <hr/>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         </Layout>
     );
